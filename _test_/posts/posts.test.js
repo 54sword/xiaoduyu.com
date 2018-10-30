8edbcba9e7b733ec37f982cb542bdfcb29123b1f
mzy-ray <mzy.ray@gmail.com>
2018-10-29 19:45:11 -0700

    jest and enzyme

---
+++ _test_/posts/posts.test.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { shallow, mount, configure } from 'enzyme';
+import renderer from 'react-test-renderer'
+import MarketAnalysis from '../../src/components/market-analysis'
+import configureStore from 'redux-mock-store'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({ adapter: new Adapter() })
+
+describe('>>>posts --- Shallow Render REACT COMPONENTS',()=>{
+    let wrapper;
+
+    beforeEach(()=>{
+        wrapper = shallow(<MarketAnalysis house={house} exchangeRate={exchangeRate} history={history}/>)
+    })
+
+    it('+++ render the DUMB component', () => {
+       expect(wrapper.length).toEqual(1)
+    });
+
+    it('+++ contains header - h2', () => {
+        expect(wrapper.contains(<div styleName="analysis-title">市场分析</div>)).toBe(true)
+    });
+});
