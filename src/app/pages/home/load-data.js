951d4f9092a78e9de25001494e018ea0664f1a85
54sword <54sword@gmail.com>
2019-05-27 11:18:20 +0800

    bug修复

--- src/app/pages/home/load-data.js
+++ src/app/pages/home/load-data.js
@@ -2,11 +2,34 @@
 import MixingFeedLoadData from '@modules/mixing-feed/load-data';
 import TopicsLoadData from '@modules/topics/load-data';
 
+import { loadPostsList } from '@actions/posts';
+
 export default ({ store, match, res, req, user }) => {
   return new Promise(async resolve => {
 
     Promise.all([
-      MixingFeedLoadData({ store, match, res, req, user }),
+      new Promise(async resolve => {
+
+        if (user) {
+          resolve();
+          return;
+        }
+
+        await loadPostsList({
+          id:'home',
+          filters: {
+            variables: {
+              sort_by: "sort_by_date",
+              deleted: false,
+              weaken: false
+            }
+          }
+        })(store.dispatch, store.getState);
+
+        resolve();
+
+      }),
+      // MixingFeedLoadData({ store, match, res, req, user }),
       TopicsLoadData({ store, match, res, req, user })
     ]).then(res=>{
       resolve({ code:200 });
